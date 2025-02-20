31a2764c44cb2dfff62d40725ed85ba24412b9f9
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-20 11:22:41 +0300

    Добавление if к первому числу

--- src/Summation.java
+++ src/Summation.java
@@ -1,7 +1,18 @@
 package src;
 
+import java.util.Scanner;
+
 public class Summation {
     public static void main(String[] args) {
+        System.out.println("Введите два числа, не превышающие  1 000 000 000");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Первое число:");
+        int numberA = scanner.nextInt();
+        if (numberA > 1000000000) {
+            System.out.println("Ошибка: Число больше 1 000 000 000");
+            return;
+        }
 
     }
 }
