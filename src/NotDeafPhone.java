de532e69317218b636747ee0ee5e2ce21c3e89a8
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-20 11:01:30 +0300

    объявление переменной inputNumber Класса Scanner

--- src/NotDeafPhone.java
+++ src/NotDeafPhone.java
@@ -1,7 +1,11 @@
 package src;
 
+import java.util.Scanner;
+
 public class NotDeafPhone {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
 
     }
 }
