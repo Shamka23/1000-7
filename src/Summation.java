b1282fda89d08cb3cf4917151fc514db9b643d2a
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-21 21:41:49 +0300

    инициализация примитивов

--- src/Summation.java
+++ src/Summation.java
@@ -1,27 +1,35 @@
 package src;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Summation {
-    public static void main(String[] args) {
-        System.out.println("Введите два числа, не превышающие  1 000 000 000");
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Первое число:");
-        int numberA = scanner.nextInt();
-        if (numberA > 1000000000) {
-            System.out.println("Ошибка: Число больше 1 000 000 000");
-            return;
+    public static void sumNumbers(){
+        int firstNumber;
+        int secondNumber;
+//        if (firstNumber || secondNumber > 1000000000){
+            
         }
-
-        System.out.println("Второе число");
-        int numberB = scanner.nextInt();
-        if (numberB > 1000000000) {
-            System.out.println("Ошибка: Число больше 1 000 000 000");
-            return;
-        }
-
-        long result = numberA + numberB;
-        System.out.println(result);
     }
+
+//    public static void main(String[] args) {
+//        System.out.println("Введите два числа, не превышающие  1 000 000 000");
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Первое число:");
+//        int numberA = scanner.nextInt();
+//        if (numberA > 1000000000) {
+//            System.out.println("Ошибка: Число больше 1 000 000 000");
+//            return;
+//        }
+//
+//        System.out.println("Второе число");
+//        int numberB = scanner.nextInt();
+//        if (numberB > 1000000000) {
+//            System.out.println("Ошибка: Число больше 1 000 000 000");
+//            return;
+//        }
+//
+//        long result = numberA + numberB;
+//        System.out.println(result);
+//    }
 }
