d5314f6325fbcaf522478a54f450c9d32f11e3b6
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-22 23:08:35 +0300

    Внедрение решения задачи

--- src/Summation.java
+++ src/Summation.java
@@ -1,35 +1,14 @@
 package src;
 
-//import java.util.Scanner;
-
 public class Summation {
-    public static void sumNumbers(){
-        int firstNumber;
-        int secondNumber;
-//        if (firstNumber || secondNumber > 1000000000){
-            
+    public static long sumNumbers(int firstNumber, int secondNumber) {
+        if (firstNumber > 1000000000 && secondNumber > 1000000000) {
+            System.out.println("Ошибка: число больше 1 000 000 000");
+        } else {
+            long result = firstNumber + secondNumber;
+            System.out.println(result);
         }
+        return 0;
     }
-
-//    public static void main(String[] args) {
-//        System.out.println("Введите два числа, не превышающие  1 000 000 000");
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Первое число:");
-//        int numberA = scanner.nextInt();
-//        if (numberA > 1000000000) {
-//            System.out.println("Ошибка: Число больше 1 000 000 000");
-//            return;
-//        }
-//
-//        System.out.println("Второе число");
-//        int numberB = scanner.nextInt();
-//        if (numberB > 1000000000) {
-//            System.out.println("Ошибка: Число больше 1 000 000 000");
-//            return;
-//        }
-//
-//        long result = numberA + numberB;
-//        System.out.println(result);
-//    }
 }
+
