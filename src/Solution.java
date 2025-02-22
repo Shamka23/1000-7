91372a5168503d5977160459eb834699eafad9fa
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-22 23:41:54 +0300

    Проверка кода.

--- src/Solution.java
+++ src/Solution.java
@@ -3,6 +3,8 @@ package src;
 
 public class Solution {
     public static void main(String[] args) {
-
+        NotDeafPhone.infoOutput(102);
+        NotDeafPhone.infoOutput(2);
+        NotDeafPhone.infoOutput(98);
     }
 }
