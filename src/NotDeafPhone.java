f296df49218f328787fbec98187cf52af450b611
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-22 23:41:41 +0300

    Добавление решения

--- src/NotDeafPhone.java
+++ src/NotDeafPhone.java
@@ -1,16 +1,15 @@
 package src;
 
-import java.util.Scanner;
 
 public class NotDeafPhone {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int inputNumber = scanner.nextInt();
-
-        if (inputNumber <= 1 || inputNumber >= 100){
-            System.out.println("Числа не в том диапазоне, попробуй от 1 до 100");
-        } else {
-            System.out.println(inputNumber);
+    public static void infoOutput(int inputNumber){
+        while (true) {
+            if (inputNumber > 100 || inputNumber < 1) {
+                System.out.println("Числа не в том диапазоне. Попробуйте от 1 до 100");
+            } else {
+                System.out.println(inputNumber);
+            }
+            break;
         }
     }
 }
