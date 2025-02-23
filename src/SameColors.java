c78c6e77a850c11f943d574bc7bd600938da603d
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-02-23 19:28:55 +0300

    Добавление проверки кода и цикла для проверки

--- src/SameColors.java
+++ src/SameColors.java
@@ -2,10 +2,12 @@ package src;
 
 public class SameColors {
     public static int minNumberBeadOneColor(int numberOfColors) {
-        if (numberOfColors < 1 || numberOfColors > 1000000000) {
-            return 0;
-        } else {
-            return numberOfColors + 1;
+        while (true) {
+            if (numberOfColors < 1 || numberOfColors > 1000000000) {
+                return 0;
+            } else {
+                return numberOfColors + 1;
+            }
         }
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,9 @@
 package src;
 
 public class Solution {
-
+    public static void main(String[] args) {
+            System.out.println(SameColors.minNumberBeadOneColor(13));
+            System.out.println(SameColors.minNumberBeadOneColor(1000000006));
+            System.out.println(SameColors.minNumberBeadOneColor(-10));
+    }
 }
