3045101f6730c6786e45c3e59a7f4b86d06482b1
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-03-25 15:25:22 +0300

    Добавление цикла для быстрой проверки кода

---
+++ src/Unicode.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char[] symbols = new char[9];
+        init(symbols);
+        System.out.println(new String(symbols));
+    }
+
+    public static void init(char[] symbols) {
+        {
+            char[] values = {'\u00A9', '\u004A', '\u0061', '\u0076', '\u0061', '\u0052', '\u0075', '\u0073', '\u0068'};
+
+            for (int i = 0; i < symbols.length; i++) {
+                symbols[i] = values[i];
+            }
+        }
+    }
+}
