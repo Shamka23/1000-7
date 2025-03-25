126ddf4f0d7cd7f1c53aa8b2a95728ffc0d599ad
Robert Oguzov <robertoguzov2@gmail.com>
2025-03-25 16:21:55 +0300

    Main

---
+++ ru/makaron/java/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main
+ *
+ * @author Robert Oguzov
+ * <p>
+ * 25.03.2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
