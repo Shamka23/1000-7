60c10d235136a97856e442eaf8a12220e6c099f0
Shamka23 <166367789+Shamka23@users.noreply.github.com>
2025-04-01 17:46:15 +0300

    Выполнил проверку Протокола сайтов

---
+++ ru/makaron/java/src/CheckingTheURL/CheckURL.java
@@ -0,0 +1,28 @@
+package CheckingTheURL;
+
+public class CheckURL {
+    public static void main(String[] args) {
+        String[] urls = {"https://javarush.ru", "https://google.com", "http://wikipedia.org", "facebook.com", "https://instagram", "codegym.cc"};
+        for (String url : urls) {
+            String protocol = checkProtocol(url);
+            String domain = checkDomain(url);
+
+            System.out.println("У URL-адреса - " + url + ", сетевой протокол - " + protocol + ", домен - " + domain);
+        }
+    }
+
+    public static String checkProtocol(String url) {
+        if (url.startsWith("https")) {
+            return "https";
+        } else if (url.startsWith("http"))
+            return "http";
+        else {
+            return "неизвестный";
+        }
+    }
+
+    public static String checkDomain(String url) {
+        //напишите тут ваш код
+        return "неизвестный";
+    }
+}
