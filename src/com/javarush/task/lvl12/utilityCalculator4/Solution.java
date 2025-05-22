2a1ad5b0ca093feae3607cd37946426ea014b464
Shama <www.shamil.com91@gmail.com>
2025-05-22 13:14:52 +0300

    Исправил выведенные ошибки

--- src/com/javarush/task/lvl12/utilityCalculator4/Solution.java
+++ src/com/javarush/task/lvl12/utilityCalculator4/Solution.java
@@ -1,7 +1,7 @@
 package com.javarush.task.lvl12.utilityCalculator4;
 
 public class Solution {
-    public static final String EQUAL = " = ";
+    private static final String EQUAL = " = ";
 
     public static void main(String[] args) {
         int a = 45;
@@ -12,21 +12,21 @@ public class Solution {
         Calculator.divide(a, b);
     }
 
-    public static class Calculator{
+    private static class Calculator{
 
-        public static void add(int a, int b) {
+        private static void add(int a, int b) {
             System.out.println(a + " + " + b + EQUAL + (a + b));
         }
 
-        public static void subtract(int a, int b) {
+        private static void subtract(int a, int b) {
             System.out.println(a + " - " + b + EQUAL + (a - b));
         }
 
-        public static void multiply(int a, int b) {
+        private static void multiply(int a, int b) {
             System.out.println(a + " * " + b + EQUAL + (a * b));
         }
 
-        public static void divide(int a, int b) {
+        private static void divide(int a, int b) {
             System.out.println(a + " / " + b + EQUAL + (a / b));
         }
     }
