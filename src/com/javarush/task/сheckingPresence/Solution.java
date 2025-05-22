006c38dabaf5b64e49dea8de12c3aa30271069b6
Shama <www.shamil.com91@gmail.com>
2025-05-22 17:45:23 +0300

    Исправил выведенные ошибки c HashSet

--- src/com/javarush/task/сheckingPresence/Solution.java
+++ src/com/javarush/task/сheckingPresence/Solution.java
@@ -6,13 +6,14 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class Solution {
-    private static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
-    private static String[] searchingWords = {"Яблоко", "Грейпфрут", "Ананас"};
-    private static HashSet<String> hashSetWhichNecessaryToFindSearchingWords = new HashSet<>(List.of("Суетолог копал и нашёл клад 3 арбуза 1 Яблоко Грейпфрут и Ананас".split(" ")));
+    private static final String[] STRINGWORDS = "Суетолог копал и нашёл клад 3 арбуза 1 Яблоко Грейпфрут и Ананас".split(" ");
+    private static final HashSet<String> WORDS = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
+    private static final String[] SEARCHING_WORDS = {"Яблоко", "Грейпфрут", "Ананас"};
+    private static final HashSet<String> HASH_SET_WHICH_NECESSARY_TO_FIND_SEARCHING_WORDS = new HashSet<>(List.of(STRINGWORDS));
 
 
     private static void checkWords(String word) {
-        if (words.contains(word)) {
+        if (WORDS.contains(word)) {
             System.out.println("Слово " + word + " есть в множестве");
         } else {
             System.out.println("Слова " + word + " нет в множестве");
@@ -20,7 +21,6 @@ public class Solution {
     }
 
     private static boolean containsArray(String[] array, HashSet<String> hashSet) {
-        hashSet.containsAll(asList(array));
         int count = 0;
         for (String element : array) {
             if (hashSet.contains(element)) {
@@ -33,7 +33,7 @@ public class Solution {
     public static void main(String[] args) {
         checkWords("JavaScript");
         checkWords("Java");
-        boolean resultArrayToHashMove = containsArray(searchingWords, hashSetWhichNecessaryToFindSearchingWords);
+        boolean resultArrayToHashMove = containsArray(SEARCHING_WORDS, HASH_SET_WHICH_NECESSARY_TO_FIND_SEARCHING_WORDS);
         System.out.println(resultArrayToHashMove);
     }
 }
