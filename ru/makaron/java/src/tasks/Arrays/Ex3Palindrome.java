3ebed3b2f3e77c9737b61af1c430fb12f6805d50
Shama <www.shamil.com91@gmail.com>
2025-05-28 09:48:10 +0300

    Добавил в параметры метода checkWords HashSet

--- ru/makaron/java/src/tasks/Arrays/Ex3Palindrome.java
+++ ru/makaron/java/src/tasks/Arrays/Ex3Palindrome.java
@@ -2,12 +2,12 @@ package tasks.Arrays;
 
 public class Ex3Palindrome {
     public static void main(String[] args) {
-        int[] array = {3,2,1,2,3};
+        int[] array = {3, 2, 1, 2, 3};
         boolean isPalindrome = findPalindrome(array);
         System.out.println(isPalindrome);
     }
 
-    private static boolean findPalindrome(int[] arrayNumbers)  {
+    private static boolean findPalindrome(int[] arrayNumbers) {
         for (int i = 0; i < arrayNumbers.length / 2; i++) {
             if (arrayNumbers[i] != arrayNumbers[arrayNumbers.length - 1 - i]) {
                 return false;
