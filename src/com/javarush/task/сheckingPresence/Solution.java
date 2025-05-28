1724ef27af0196bc355a9607a508f634796b6f77
Shama <www.shamil.com91@gmail.com>
2025-05-28 09:39:16 +0300

    Добавил в параметры метода checkWords HashSet

--- src/com/javarush/task/сheckingPresence/Solution.java
+++ src/com/javarush/task/сheckingPresence/Solution.java
@@ -6,14 +6,14 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class Solution {
-    private static final String[] STRINGWORDS = "Суетолог копал и нашёл клад 3 арбуза 1 Яблоко Грейпфрут и Ананас".split(" ");
+    private static final String[] STRING_WORDS = "Суетолог копал и нашёл клад 3 арбуза 1 Яблоко Грейпфрут и Ананас".split(" ");
     private static final HashSet<String> WORDS = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
     private static final String[] SEARCHING_WORDS = {"Яблоко", "Грейпфрут", "Ананас"};
-    private static final HashSet<String> HASH_SET_WHICH_NECESSARY_TO_FIND_SEARCHING_WORDS = new HashSet<>(List.of(STRINGWORDS));
+    private static final HashSet<String> HASH_SET_WHICH_NECESSARY_TO_FIND_SEARCHING_WORDS = new HashSet<>(List.of(STRING_WORDS));
 
 
-    private static void checkWords(String word) {
-        if (WORDS.contains(word)) {
+    private static void checkWords(HashSet<String> wordSet, String word) {
+        if (wordSet.contains(word)) {
             System.out.println("Слово " + word + " есть в множестве");
         } else {
             System.out.println("Слова " + word + " нет в множестве");
@@ -31,8 +31,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        checkWords("JavaScript");
-        checkWords("Java");
+        checkWords(WORDS,"JavaScript");
+        checkWords(WORDS,"Java");
         boolean resultArrayToHashMove = containsArray(SEARCHING_WORDS, HASH_SET_WHICH_NECESSARY_TO_FIND_SEARCHING_WORDS);
         System.out.println(resultArrayToHashMove);
     }
